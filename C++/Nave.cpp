03c71f45952c0f553d85564a2122d0bfca230d90
Mario <enigsea@gmail.com>
2013-09-16 23:13:27 -0500

    the class 'Asteroide' is added.

--- C++/Nave.cpp
+++ C++/Nave.cpp
@@ -128,3 +128,15 @@ void nave::morir() {
 		pintar();							
 	}
 }
+
+class Asteroide {
+	int x,y;
+	
+	public:
+		Asteroide(int _x, int _y):x(_x), y(_y) {}
+		int X() { return x; }
+		int Y() { return y; }
+		void pintar();
+		void mover();
+		void choque(nave &N);
+};
