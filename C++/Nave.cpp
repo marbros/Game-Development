f90660a851b7985e2ddabdfe91a3091fb05e6b8b
Mario <enigsea@gmail.com>
2013-09-17 11:29:58 -0500

    the method 'choque' is added

--- C++/Nave.cpp
+++ C++/Nave.cpp
@@ -154,3 +154,14 @@ void Asteroide::mover() {
 	}
 	pintar();
 }
+
+void Asteroide::choque(nave &N) {
+	if ((x >= N.X() && x < N.X()+6) && (y >= N.Y() && y <= N.Y()+2)) {
+		N.COR();
+		N.borrar();
+		N.pintar();
+		N.pintar_corazones();
+		x = rand()%71 + 4;
+		y = 4;		
+	}
+}
