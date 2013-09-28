2b023ae64eb87211f19a4f4deca2033bc748bcda
Mario <enigsea@gmail.com>
2013-09-27 22:54:40 -0500

    the method paint 'pieza' is added.

--- C++/Tetris/Tetris.cpp
+++ C++/Tetris/Tetris.cpp
@@ -12,6 +12,14 @@ void cuadrado(int x, int y, int col) {
 					 y * TAM + TAM);
 }
 
+void pieza_s1(int x, int y) {
+	color(VERDE);
+	cuadrado(x, y);
+	cuadrado(x + 1, y + 0);
+	cuadrado(x + 1, y + 1);
+	cuadrado(x + 2, y + 1);
+}
+
 int main() {
 	vredimensiona(TAM * 10, TAM * 20);
 	for(int i = 0; i < 10; i++) {
