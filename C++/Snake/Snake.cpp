0df9ab9f909f02c3a48a33ff4f0c4bb47bc980a7
Mario <enigsea@gmail.com>
2013-10-19 22:17:33 -0500

    the method 'pintar' is added.

--- C++/Snake/Snake.cpp
+++ C++/Snake/Snake.cpp
@@ -1,4 +1,4 @@
-#inclide <windows.h>
+#include <windows.h>
 #include <iostream>
 #include <conio.h>
 
@@ -18,8 +18,26 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(hCon,dwPos);
 }
 
+void pintar() {
+	//lineas hori
+	for (int i = 2; i < 78; ++i) {
+	 	gotoxy(i,3); printf("%c", 205);
+		gotoxy(i,23); printf("%c", 205);
+	}
+	//lineas Ver
+	for (int i = 4; i < 23; ++i) {
+	 	gotoxy(2,i); printf("%c", 186);
+		gotoxy(77,i); printf("%c", 186);
+	}
+	//Esquinas
+	gotoxy(2,3); printf("%c",201);
+	gotoxy(2,23); printf("%c",200);
+	gotoxy(77,3); printf("%c",187);
+	gotoxy(77,23); printf("%c",188);
+}
+
 int main() {
-	
-	system("pause");
+	pintar();
+	system("pause>null");
 	return 0;
 }
