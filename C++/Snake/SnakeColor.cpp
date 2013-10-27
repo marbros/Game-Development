ea6df66361b0103e912239d13a4334f5d8e15083
Mario <enigsea@gmail.com>
2013-10-26 22:32:17 -0500

    the method 'pintar' is added.

--- C++/Snake/SnakeColor.cpp
+++ C++/Snake/SnakeColor.cpp
@@ -23,6 +23,26 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(hCon,dwPos);
 }
 
+void pintar() {
+	//lineas hori
+	for (int i = 2; i < 78; ++i) {
+	 	gotoxy(i,3);
+	 	printf("%c", 205);
+		gotoxy(i,23);
+		printf("%c", 205);
+	}
+	//lineas Ver
+	for (int i = 4; i < 23; ++i) {
+	 	gotoxy(2,i); printf("%c", 186);
+		gotoxy(77,i); printf("%c", 186);
+	}
+	//Esquinas
+	gotoxy(2,3); printf("%c",201);
+	gotoxy(2,23); printf("%c",200);
+	gotoxy(77,3); printf("%c",187);
+	gotoxy(77,23); printf("%c",188);
+}
+
 void cuadrado(const Punto& p, int c) {
 	color(c);
 	rectangulo_lleno(p.x*SZ, p.y*SZ, (p.x+1)*SZ-1, (p.y+1)*SZ-1);
@@ -51,6 +71,7 @@ Punto al_azar() {
 
 int main() {
 	vredimensiona(XTAM * SZ, YTAM * SZ);
+	pintar();
 	Punto cabeza = {30, 20};
 	Punto comida = al_azar();
 	int vx = 1, vy = 0; //velocidad
