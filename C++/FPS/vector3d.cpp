de26fb90020bc21ee2580d735e5ffb805f3456bb
Mario <enigsea@gmail.com>
2013-10-29 00:41:50 -0500

    three methods are added to file 'vector3d.cpp'

--- C++/FPS/vector3d.cpp
+++ C++/FPS/vector3d.cpp
@@ -1,5 +1,22 @@
 #include "vector3d.h"
 
+vector3d::vector3d() {
+	x = y = z = 0;
+}
+
+vector3d::vector3d(float a, float b)
+{
+	x = a;
+	y = b;
+	z = 0;
+}
+
+vector3d::vector3d(float a, float b, float c) {
+	x = a;
+	y = b;
+	z = c;
+}
+
 vector3d::vector3d();
 	  vector3d::vector3d(float a, float b);
 	  vector3d::vector3d(float a, float b, float c);
