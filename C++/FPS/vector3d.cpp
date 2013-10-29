3aa19b8be7fb79ffdfcee43a2b0da32ec97bda8f
Mario <enigsea@gmail.com>
2013-10-29 00:37:35 -0500

    the file 'vector3d' is added

---
+++ C++/FPS/vector3d.cpp
@@ -0,0 +1,36 @@
+#include "vector3d.h"
+
+vector3d::vector3d();
+	  vector3d::vector3d(float a, float b);
+	  vector3d::vector3d(float a, float b, float c);
+
+	  float vector3d::dotProduct(const vector3d& vec2);
+	  vector3d vector3d::crossProduct(const vector3d& vec2);
+	  float vector3d::normalize();
+
+	  void vector3d::change(float a, float b, float c);
+	  void vector3d::change(vector3d& vec2);
+	  void vector3d::change(vector3d vec2);
+	  void vector3d::changeX(float a);
+	  void vector3d::changeY(float a);
+	  void vector3d::changeZ(float a);
+
+	  vector3d vector3d::operator+(const vector3d& vec2);
+	  vector3d vector3d::operator-(const vector3d& vec2);
+	  vector3d vector3d::operator*(float vec2);
+	  vector3d vector3d::operator/(float vec2);
+
+	  vector3d& vector3d::operator+=(const vector3d& vec2);
+	  vector3d& vector3d::operator-=(const vector3d& vec2);
+	  vector3d& vector3d::operator*=(float vec2);
+	  vector3d& vector3d::operator/=(float vec2);
+
+	  bool vector3d::operator==(const vector3d vec2);
+	  bool vector3d::operator!=(const vector3d vec2);
+
+	  cout << vec;
+
+	  friend std::ostream& operator<<(std::ostream& out, const vector3d& vec);
+};
+
+#endif
