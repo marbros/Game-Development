0a994f86a7cdc8dda5ac324739d814526daebd6d
Mario <enigsea@gmail.com>
2013-10-31 23:23:57 -0500

    some changes in the file gameserver are added

--- Js/Snake/server/GameServer.js
+++ Js/Snake/server/GameServer.js
@@ -7,4 +7,60 @@ var GameServer = function (options) {
     this.initialize(options);
 };
 
+GameServer.prototype = {
+
+    initialize : function (options) {
+        this.io = options.io;
+
+        this.board = new Board();
+        this.players = new PlayerCollection();
+        this.foodCollection = new FoodCollection();
+
+        this.updateDataFields = {players : true};  //information to send on the next update event
+
+        this.bindModelEvents();
+        this.bindSocketsEvents();
+    },
+
+    bindModelEvents : function () {
+    },
+
+    bindSocketsEvents : function () {
+    },
+
+    onSocketAddPlayer : function (socket, username) {
+    },
+
+    onSocketDisconnect : function (socket) {
+    },
+
+    onSocketKeydown : function (socket, key) {
+    },
+
+    detectCollisions : function () {
+
+    },
+
+    updateData : function () {
+
+    },
+
+    gameLoop : function () {
+
+    },
+
+    addFood : function () {
+
+    },
+
+    startIntervals : function () {
+
+    },
+
+    stopIntervals : function () {
+
+    }
+
+};
+
 module.exports = GameServer;
