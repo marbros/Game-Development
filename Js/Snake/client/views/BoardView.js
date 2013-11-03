564bebba9d0fe9a87cb9afef9a9f0dfd567fedf7
Mario <enigsea@gmail.com>
2013-11-03 00:15:41 -0500

    new changes in the view of board are added

--- Js/Snake/client/views/BoardView.js
+++ Js/Snake/client/views/BoardView.js
@@ -3,6 +3,19 @@
 
     Game.BoardView = Backbone.View.extend({
 
+        initialize : function (options) {
+            this.ctx = options.ctx;
+            this.board = options.board;
+            this.listenTo(this.board, "change", this.render);
+        },
+
+        draw : function () {
+            var cellWidth = this.board.get('cellWidth');
+            var cellHeight = this.board.get('cellHeight');
+            var boardWidth = this.board.get('x') * cellWidth;
+            var boardHeight = this.board.get('y') * cellHeight;
+        },
+
     });
 
 }());
