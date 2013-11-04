83202c98d3cb7b69b126895dee36953d612d5a91
Mario <enigsea@gmail.com>
2013-11-03 23:24:09 -0500

    the view of client side 'Food' has been completed

--- Js/Snake/client/views/FoodView.js
+++ Js/Snake/client/views/FoodView.js
@@ -1,3 +1,29 @@
 Game.FoodView = Backbone.View.extend({
 
+    initialize : function (options) {
+        this.ctx = options.ctx;
+        this.board = options.board;
+        this.food = options.food;
+
+        this.listenTo(this.food, "reset", this.render);
+    },
+
+    draw : function () {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+        var cellWidth = this.board.get('cellWidth');
+        var cellHeight = this.board.get('cellHeight');
+
+        this.ctx.fillStyle = "#D8FD06";
+        this.food.each(function (f) {
+            var x = f.get('x');
+            var y = f.get('y');
+            this.ctx.fillRect(x * cellWidth + 0.5, y * cellHeight + 0.5, cellWidth, cellHeight );
+        }, this);
+    },
+
+    render : function () {
+        requestAnimationFrame(_.bind(this.draw, this));
+    }
+
 });
