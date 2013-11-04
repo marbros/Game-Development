416a6b7653f517825bb2d4cdaf6b2e3b6caf03fb
Mario <enigsea@gmail.com>
2013-11-03 23:14:38 -0500

    new changes in the view of model are added

--- Js/Snake/client/views/BoardView.js
+++ Js/Snake/client/views/BoardView.js
@@ -14,6 +14,18 @@
             var cellHeight = this.board.get('cellHeight');
             var boardWidth = this.board.get('x') * cellWidth;
             var boardHeight = this.board.get('y') * cellHeight;
+
+            for (var x = 0; x <= this.board.get('x'); x++) {
+                this.ctx.moveTo(0.5 + x * cellWidth, 0);
+                this.ctx.lineTo(0.5 + x * cellWidth, boardHeight);
+            }
+
+            for (var y = 0; y <= this.board.get('y'); y++) {
+                this.ctx.moveTo(0, 0.5 + y * cellHeight);
+                this.ctx.lineTo(boardWidth, 0.5 + y * cellHeight);
+            }
+            this.ctx.strokeStyle = "#074547";
+            this.ctx.stroke();            
         },
 
     });
