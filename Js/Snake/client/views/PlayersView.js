65714574341756b404eb4f7de2b39a69100bdc76
Mario <enigsea@gmail.com>
2013-11-05 00:04:18 -0500

    new changes in the view of Player are added to side client

--- Js/Snake/client/views/PlayersView.js
+++ Js/Snake/client/views/PlayersView.js
@@ -3,6 +3,17 @@
 
     Game.PlayersView = Backbone.View.extend({
 
+        initialize : function (options) {
+            this.ctx = options.ctx;
+            this.players = options.players;
+            this.board = options.board;
+            this.listenTo(this.players, 'reset', this.render);
+        },
+
+        color : function (player) {
+            return player.get('username') === Game.username ? "#00FEFF" : "#FF0065";
+        },
+
     });
 
 }());
