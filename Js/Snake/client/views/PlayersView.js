44c6f3b7b043a11015450a81ff0c555a2c1538d1
Mario <enigsea@gmail.com>
2013-11-05 00:05:54 -0500

    the view of client side 'Player' has been completed

--- Js/Snake/client/views/PlayersView.js
+++ Js/Snake/client/views/PlayersView.js
@@ -14,6 +14,24 @@
             return player.get('username') === Game.username ? "#00FEFF" : "#FF0065";
         },
 
+        draw : function () {
+            var self = this;
+            var cellWidth = this.board.get('cellWidth');
+            var cellHeight = this.board.get('cellHeight');
+
+            this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+            this.players.each(function (player) {
+                this.ctx.fillStyle = self.color(player);
+                _.each(player.get('parts'), function (part) {
+                    this.ctx.fillRect(part.x * cellWidth, part.y * cellHeight, cellWidth, cellHeight);
+                }, this);
+            }, this);
+        },
+
+        render : function () {
+            requestAnimationFrame(_.bind(this.draw, this));
+        }
+
     });
 
 }());
