388729eefe7fefa6950ba7c8926ae0ce759fad2e
Mario <enigsea@gmail.com>
2013-11-08 00:23:06 -0500

    the function 'movePosition' is added to side client

--- Js/Snake/server/models/Player.js
+++ Js/Snake/server/models/Player.js
@@ -22,7 +22,29 @@ var Player = Backbone.Model.extend({
     },
 
     movePosition : function () {
+        var board = this.get('board');
 
+        var newPart = _.clone(this.attributes.parts[0]);
+
+        this.calculateNewDirection();
+        newPart.x = newPart.x + this.attributes.direction.x;
+        newPart.y = newPart.y + this.attributes.direction.y;
+
+        // control game limits
+        if (newPart.x < 0) {
+            newPart.x = board.get('x');
+        } else if (newPart.x >= board.get('x')) {
+            newPart.x = 0;
+        }
+
+        if (newPart.y < 0) {
+            newPart.y = board.get('y');
+        } else if (newPart.y >= board.get('y')) {
+            newPart.y = 0;
+        }
+
+        this.lastRemovedPart = this.attributes.parts.pop();
+        this.attributes.parts.unshift(newPart);
     },
 
     calculateNewDirection : function () {
