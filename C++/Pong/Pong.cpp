873be61b008e8d72d4426f52892c2b6fa1cd2a20
Mario <enigsea@gmail.com>
2013-11-12 23:59:13 -0500

    the methods 'borrar' and 'pintar' has been completed

--- C++/Pong/Pong.cpp
+++ C++/Pong/Pong.cpp
@@ -12,9 +12,22 @@ class JUGADOR {
 JUGADOR::JUGADOR(int _x, int _y):x(_x), y(_y) {}
 
 void JUGADOR::pintar() const {
-	gotoxy(x, y-1);
+	gotoxy(x, y-1); printf("%c", 219);
+	gotoxy(x, y); printf("%c", 219);
+	gotoxy(x, y+1); printf("%c", 219);	
+}
+
+void JUGADOR::borrar() const {
+	gotoxy(x, y-1); printf("%c", " ");
+	gotoxy(x, y); printf("%c", " ");
+	gotoxy(x, y+1); printf("%c", " ");	
 }
 
 int main() {
 	
+	JUGADOR A(6,15);
+	A.pintar();
+	JUGADOR B(74,15);
+	B.pintar();
+	return 0;
 }
