1c021ed542706083d050f9de65059d22c87edae0
Mario <enigsea@gmail.com>
2013-11-24 23:01:05 -0500

    the move of circle is added

--- C++/Rebound/Rebound.cpp
+++ C++/Rebound/Rebound.cpp
@@ -4,7 +4,16 @@ using namespace miniwin;
 int main() {
 	vredimensiona(800, 600);
 	int x = 200, y= 200;
-
+	for (int i = 0; i < 200; ++i)
+	{
+		/* Pasos para crear una Animacion borrar, pintar, refrescar */
+		borra();
+		circulo(x, y, 30);
+		refresca();
+		x += 2;
+		y += 2;
+		espera(10);
+	}
 	circulo(200, 200, 30);
 	refresca();
 	return 0;
