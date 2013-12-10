22a4d45e6220fca5c148b9a3f23477c70fc07641
Mario <enigsea@gmail.com>
2013-12-10 01:00:13 -0500

    the function 'rayplane' is added to class collision

--- C++/FPS/collision.h
+++ C++/FPS/collision.h
@@ -1,1 +1,17 @@
-collision
+#ifndef COLLISION_H
+#define COLLISION_H
+
+class collision
+{
+	bool raysphere(float xc, float yc, float zc, float xd, floar yd,
+				   float zd, float xs, float ys, float zs, float r,
+				   float *dist =NULL, coordinate* point=NULL);
+
+	bool rayplane(float nx, float ny, float nz, float xs, floar ys,
+				  float zs, float xd, float yd, float zd, coordinate p1,
+				  coordinate p2, coordinate p3, coordinate p4,
+				  float *dist =NULL, coordinate* point=NULL);
+
+};
+
+#endif
