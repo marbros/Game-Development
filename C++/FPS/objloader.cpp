63ba94cf388f8e920956759e482040a4b582de79
Mario <enigsea@gmail.com>
2013-12-21 00:42:51 -0500

    the function 'face' is added

--- C++/FPS/objloader.cpp
+++ C++/FPS/objloader.cpp
@@ -7,6 +7,18 @@
 		z=c;
 	}
 
+	face::face(int facen,int f1,int f2,int f3,int t1,int t2,int t3,int m){
+		facenum=facen;
+		faces[0]=f1;
+		faces[1]=f2;
+		faces[2]=f3;
+		texcoord[0]=t1;
+		texcoord[1]=t2;
+		texcoord[2]=t3;
+		mat=m;
+		four=false;
+	}
+
 	material::material(const char* na,float al,float n,float ni2,float* d,float* a,float* s,int i,int t)
 	{
 		name=na;
