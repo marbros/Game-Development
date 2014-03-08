ec0a8c10ce9772b93ac7c23610bcb11c4099b881
Mario <enigsea@gmail.com>
2014-03-08 02:19:08 -0500

    the class 'Node' is added to project

---
+++ Java/Node.java
@@ -0,0 +1,27 @@
+public class Node {
+  
+  // x, y, and distance
+  private int x, y; 
+  private float d;
+  
+  public Node( int x, int y, float d ){
+    
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    
+  }
+  
+  public int getX(){
+    return x;
+  }
+  
+  public int getY(){
+    return y;
+  }
+  
+  public float getD(){
+    return d;
+  }
+  
+}
