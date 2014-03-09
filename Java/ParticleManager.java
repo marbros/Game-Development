15aa08427f424b7cc544767910bca6328d3a40b7
Mario <enigsea@gmail.com>
2014-03-09 02:12:31 -0500

    the method 'colorRGBA' of the class ParticleManager is added

--- Java/ParticleManager.java
+++ Java/ParticleManager.java
@@ -15,4 +15,21 @@
  
         rand = new Random();
     }
+
+    public ColorRGBA hsvToColor(float h, float s, float v) {
+        if (h == 0 && s == 0) {
+            return new ColorRGBA(v, v, v,1);
+        }
+     
+        float c = s * v;
+        float x = c * (1 - Math.abs(h % 2 - 1));
+        float m = v - c;
+     
+        if (h < 1) {return new ColorRGBA(c + m, x + m, m, 1);
+        } else if (h < 2) {return new ColorRGBA(x + m, c + m, m, 1);
+        } else if (h < 3) {return new ColorRGBA(m, c + m, x + m, 1);
+        } else if (h < 4) {return new ColorRGBA(m, x + m, c + m, 1);
+        } else if (h < 5) {return new ColorRGBA(x + m, m, c + m, 1);
+        } else {return new ColorRGBA(c + m, m, x + m, 1);}
+    }    
 }
